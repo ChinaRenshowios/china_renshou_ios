312eb01be4b3a3e17ada2cd8abdb76045fc929ad
王智刚 <zhigang.wang@corp.elong.com>
2015-08-31 19:45:05 +0800

    修改pch路径,添加网络层文件夹

---
+++ Alumnus/AlumnusBase/AlumusNet/ZG_NetWorkManager.h
@@ -0,0 +1,13 @@
+//
+//  ZG_NetWorkManager.h
+//  Alumnus
+//
+//  Created by user on 15/8/31.
+//  Copyright (c) 2015年 wiipu. All rights reserved.
+//  网络层管理
+
+#import <Foundation/Foundation.h>
+
+@interface ZG_NetWorkManager : NSObject
+
+@end
