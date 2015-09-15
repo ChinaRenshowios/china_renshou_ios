c2e5b52259f6222b9d4261a8f6f8b44ae5a575ad
siyukang <ksy@192.168.1.104>
2015-09-15 18:20:04 +0800

    首页按钮
    首页按钮

--- Alumnus/utils/CommonVC/KSYCommonNaviViewController.m
+++ Alumnus/utils/CommonVC/KSYCommonNaviViewController.m
@@ -1,22 +1,25 @@
 //
-//  HomepageMainViewController.m
+//  KSYCommonNaviViewController.m
 //  Alumnus
 //
-//  Created by 张超 on 15/8/17.
+//  Created by ksy on 15/9/15.
 //  Copyright (c) 2015年 wiipu. All rights reserved.
 //
 
-#import "HomepageMainViewController.h"
+#import "KSYCommonNaviViewController.h"
 
-@interface HomepageMainViewController ()
+@interface KSYCommonNaviViewController ()
 
 @end
 
-@implementation HomepageMainViewController
+@implementation KSYCommonNaviViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self.navigationItem setTitle:_titleString];
+    [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor],NSForegroundColorAttributeName,[UIFont boldSystemFontOfSize:20],NSFontAttributeName, nil]];
+
     // Do any additional setup after loading the view.
 }
 
---
+++ Alumnus/utils/ZCButton/KSYHomePageButtonView.h
@@ -0,0 +1,17 @@
+//
+//  KSYHomePageButtonView.h
+//  Alumnus
+//
+//  Created by ksy on 15/9/15.
+//  Copyright (c) 2015年 wiipu. All rights reserved.
+//
+
+#import "ZCImageButton.h"
+
+@interface KSYHomePageButtonView : UIView
+@property (strong, nonatomic) UIImageView *imageView;
+@property (strong, nonatomic) UIButton *imageButton;
+@property (strong, nonatomic) UILabel *imageLabel;
+
+
+@end
