fcf32808ab7902e0a23acacc115298bfc77ae893
siyukang <ksy@192.168.1.104>
2015-09-17 09:46:04 +0800

    add files

---
+++ Alumnus/homepage/mydate/MydateIndexViewController.h
@@ -0,0 +1,13 @@
+//
+//  MydateIndexViewController.h
+//  Alumnus
+//
+//  Created by ksy on 15/9/17.
+//  Copyright (c) 2015年 wiipu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MydateIndexViewController : UIViewController
+
+@end
