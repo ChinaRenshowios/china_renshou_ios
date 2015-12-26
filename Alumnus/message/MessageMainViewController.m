3afe08d8e8d9c8a017253a403885b9e73b27cf36
zhigang.wang <sky15179@126.com>
2015-12-26 19:58:19 +0800

    信息中心添加新导航条

--- Alumnus/message/MessageMainViewController.m
+++ Alumnus/message/MessageMainViewController.m
@@ -11,16 +11,35 @@
 #import "MBProgressHUD+MJ.h"
 #import "ALNetWorkApi.h"
 #import "MessageModel.h"
-
+const NSUInteger btnTag = 35566;
 @interface MessageMainViewController ()<topScrollViewDelegate,UITableViewDataSource,UITableViewDelegate>
 @property (nonatomic, strong)topScrollView *top;
 @property (nonatomic, strong)UITableView *table;
 @property (nonatomic, strong)NSMutableArray *dataList;
+@property (nonatomic, strong)UIView *toPone;
 @end
 
 @implementation MessageMainViewController
 
 #pragma lazy
+- (UIView *)toPone
+{
+    if (!_toPone) {
+        _toPone = [[UIView alloc]init];
+        _toPone.backgroundColor = ALColor(19, 140, 80);
+        for (NSUInteger i = 0; i<3; i++) {
+            UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+            [btn setTitle:@"信息首页" forState:UIControlStateNormal];
+            [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+            CGFloat width = self.view.width/3;
+            btn.tag = btnTag + i;
+            [btn addTarget:self action:@selector(topSelect:) forControlEvents:UIControlEventTouchUpInside];
+            btn.frame = CGRectMake(i*width, 0, width, 39);
+            [_toPone addSubview:btn];
+        }
+    }
+    return _toPone;
+}
 
 #pragma mark - lifeCycle
 
@@ -57,6 +76,7 @@
         [self loadData];
     }];
     self.top.content = self.table;
+    [self.view addSubview:self.toPone];
 }
 
 //获取数据
@@ -83,14 +103,39 @@
     }];
 }
 
+- (void)topSelect:(UIButton *)btn
+{
+    switch (btn.tag - btnTag) {
+        case 0:
+        {
+        
+        }
+            break;
+        case 1:
+        {
+            
+        }
+            break;
+
+        case 2:
+        {
+            
+        }
+            break;
+        default:
+            break;
+    }
+}
+
 //刷新视图
 - (void)resetSubViews
 {
     NSArray *arr = @[@"公司动态",@"公司通知"];
     self.top = [[topScrollView alloc]initWithTitles:arr];
-    self.top.frame = CGRectMake(0, 64,SIZEWIDTH , SIZEHEIGHT - 64 - 50);
+    self.top.frame = CGRectMake(0, 64+39,SIZEWIDTH , SIZEHEIGHT - 64 - 50);
     self.top.delegate = self;
     self.top.content = self.table;
+    self.toPone.frame = CGRectMake(0, 64, self.view.width, 39);
     
     self.table.frame = CGRectMake(0, -13, SIZEWIDTH, SIZEHEIGHT - 64 -44 - 50);
 
